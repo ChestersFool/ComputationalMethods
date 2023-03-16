3c164bc94999f5b2ab4cbe36548a94104482f6c5
Mykhailo <kiiashchenko.mykhailo@chnu.edu.ua>
2023-03-16 18:54:16 +0200

    loop

--- Lab2_2/lab2_2.cpp
+++ Lab2_2/lab2_2.cpp
@@ -100,7 +100,7 @@ int main()
     vector<double> b = {0.1528, -0.0985, -0.2474, 0.3872};
     vector<double> x0 = {1, 1, 1, 1}, r0(4), p0(4), qk(4), Vtemp(4);
     double alfaK, betaK, norm;
-    const double EPS = 0.0000012;
+    const double EPS = 0.00000012;
 
     // A * x_0
     r0 = MatrixVectorMultiplication(A, x0);
@@ -108,49 +108,57 @@ int main()
     // PrintVector(r0);
     //  b - Ax_0
     r0 = VectorSubtraction(b, r0);
-    cout << "b - A * x_0 ";
+    cout << "b - A * x_0  ||||||  ";
     PrintVector(r0);
 
     // p_0 = r_0
     p0 = r0;
 
-    // q_k = A * p_k
-    qk = MatrixVectorMultiplication(A, p0);
-    cout << "q_k = A * p_k ";
-    PrintVector(qk);
-
-    // alfaK = (r_k, p_k) / (q_k, p_k)
-    alfaK = VectorScalar(r0, p0) / VectorScalar(qk, p0);
-    cout << "alfaK = (r_k, p_k) / (q_k, p_k) " << alfaK << '\n';
-
-    // x_k+1 = x_k + alfaK * p_k
-    Vtemp = VectorNumberMultiplication(x0, alfaK);
-    x0 = VectorSubtraction(x0, Vtemp);
-    cout << "x_k+1 = x_k + alfaK * p_k";
-    PrintVector(x0);
-
-    // r_k+1 = r_k - alfaK * q_k
-    Vtemp = VectorNumberMultiplication(qk, alfaK);
-    r0 = VectorSubtraction(r0, Vtemp);
-    cout << "r_k+1 = r_k - alfaK * q_k";
-    PrintVector(r0);
-
-    norm = vectorNorm(r0);
-    cout << "norm of r0 " << norm << '\n';
-
-    if (norm <= EPS)
+    while (true)
     {
-        // Задовільняє умову
-    }
-    else
-    {
+        // q_k = A * p_k
+        qk = MatrixVectorMultiplication(A, p0);
+        cout << "q_k = A * p_k  ||||||  ";
+        PrintVector(qk);
+
+        // alfaK = (r_k, p_k) / (q_k, p_k)
+        alfaK = VectorScalar(r0, p0) / VectorScalar(qk, p0);
+        cout << "alfaK = (r_k, p_k) / (q_k, p_k)  ||||||  " << alfaK << '\n';
+
+        // x_k+1 = x_k + alfaK * p_k
+        Vtemp = VectorNumberMultiplication(x0, alfaK);
+        x0 = VectorSubtraction(x0, Vtemp);
+        cout << "x_k+1 = x_k + alfaK * p_k  ||||||  ";
+        PrintVector(x0);
+
+        // r_k+1 = r_k - alfaK * q_k
+        Vtemp = VectorNumberMultiplication(qk, alfaK);
+        r0 = VectorSubtraction(r0, Vtemp);
+        cout << "r_k+1 = r_k - alfaK * q_k  ||||||  ";
+        PrintVector(r0);
+
+        norm = vectorNorm(r0);
+        cout << "norm of r0  ||||||  " << norm << '\n';
+
+        if (norm <= EPS)
+        {
+            cout << "EPPPPP, UNSWER IS  ||||||  ";
+            PrintVector(x0);
+            return 0;
+        }
+
+        // betak = (r_k+1, q_k) / (p_k , q_k)
         betaK = VectorScalar(r0, qk) / VectorScalar(p0, qk);
-        cout << "betaK = " << betaK << '\n';
+        cout << "betak = (r_k+1, q_k) / (p_k , q_k)  ||||||  " << betaK << '\n';
+
         // p_k+1 = r_k+1 - betaK * p_k
         Vtemp = VectorNumberMultiplication(p0, betaK);
         p0 = VectorSubtraction(r0, Vtemp);
-        cout << "p_k+1 = r_k+1 - betaK * p_k";
+        cout << "p_k+1 = r_k+1 - betaK * p_k  ||||||  ";
         PrintVector(p0);
+
+        cout << "\n -------NEXT STEP------- \n\n";
     }
+
     return 0;
 }
