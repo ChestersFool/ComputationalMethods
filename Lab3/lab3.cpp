4e2eedbf826af2aa3755128a989248a77f3dbcf9
Kiiashchenko_Mykhailo <kiiashchenko.mykhailo@chnu.edu.ua>
2023-03-17 11:11:43 +0200

    lab3

--- Lab3/lab3.cpp
+++ Lab3/lab3.cpp
@@ -10,20 +10,45 @@ double Foo(double r, double ro, double h)
 
 double FooDerivative(double r, double ro, double h)
 {
-    return 2 * pow(h, 2) - 6 * r * h + 12 * pow(r, 2) * ro;
+    return 3 * pow(h, 2) - 6 * r * h;
 }
 
 int main()
 {
     // h ^ 3 - 3 * r * h ^ 2 + 4 * r ^ 3 * ro = 0
-    double x0 = 0, xk = 0, xk1, ro = 0.620, r = 10;
+    double xk = 0, xk1 = 28, xkminus1 = 28, ro = 0.620, r = 10, eps = 0.001, res1, res2;
+    int i = 0;
 
-    for (int i = 0; i < 10; i++)
+    do
     {
-        xk = xk - Foo(r, ro, i) / FooDerivative(r, ro, i);
+        xk = xk1;
+        res1 = Foo(r, ro, xk);
+        res2 = FooDerivative(r, ro, xk);
+        xk1 = xk - res1 / res2;
 
-        cout << "n = " << i << "; x = " << xk << "; f[x] = " << Foo(r, ro, i) << "; f1[x] = " << FooDerivative(r, ro, i) << '\n';
-    }
+        cout << "n = " << i << "; x = " << xk << "; f[x] = " << res1 << "; f1[x] = " << res2 << '\n';
+        i++;
+    } while (fabs(xk - xk1) > eps); // while(i < 10);//
+
+    cout << "\n----------\n\n";
+
+    xk = 0;
+    xk1 = 28;
+    xkminus1 = 28;
+    i = 0;
+
+    do
+    {
+        xkminus1 = xk;
+        xk = xk1;
+
+        res1 = Foo(r, ro, xk);
+        res2 = Foo(r, ro, xkminus1);
+        xk1 = xk - (res1 * (xk - xkminus1)) / (res1 - res2);
+
+        cout << "n = " << i << "; x = " << xk << "; f[x] = " << res1 << "; f1[x] = " << res2 << '\n';
+        i++;
+    } while (fabs(xk - xk1) > eps);
 
     return 0;
 }
